27736d8ce7da87d0c61ec9dd0449a5b492d5ccf2
Neal Sanche <thorinside@gmail.com>
2015-03-27 19:38:25 -0600

    Let's Butterknife it!

--- wear/src/main/java/org/nsdev/wearableapp/MyDisplayActivity.java
+++ wear/src/main/java/org/nsdev/wearableapp/MyDisplayActivity.java
@@ -2,17 +2,29 @@ package org.nsdev.wearableapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MyDisplayActivity extends Activity {
 
-    private TextView mTextView;
+    @InjectView(R.id.text)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-        mTextView = (TextView) findViewById(R.id.text);
+
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.root)
+    public void onRootClicked(View root) {
+        // Do something
+        mTextView.setText("Tapped.");
     }
 }
