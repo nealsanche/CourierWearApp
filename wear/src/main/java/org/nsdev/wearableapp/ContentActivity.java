3eb0149b28a22685b923caf436d0f8e294fbffa6
Neal Sanche <thorinside@gmail.com>
2015-03-27 20:30:21 -0600

    Adds the long-press to dismiss pattern.

--- wear/src/main/java/org/nsdev/wearableapp/ContentActivity.java
+++ wear/src/main/java/org/nsdev/wearableapp/ContentActivity.java
@@ -2,7 +2,10 @@ package org.nsdev.wearableapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.DismissOverlayView;
 import android.support.wearable.view.WatchViewStub;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
@@ -11,12 +14,18 @@ import butterknife.Optional;
 
 public class ContentActivity extends Activity {
 
+    @InjectView(R.id.watch_view_stub)
+    WatchViewStub mStub;
+
     @Optional
     @InjectView(value = R.id.text)
     TextView mTextView;
 
-    @InjectView(R.id.watch_view_stub)
-    WatchViewStub mStub;
+    @Optional
+    @InjectView(R.id.dismiss_overlay)
+    DismissOverlayView mDismissOverlayView;
+
+    private GestureDetector mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,22 @@ public class ContentActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 ButterKnife.inject(ContentActivity.this, mStub);
+
+                mDismissOverlayView.setIntroText(R.string.dismiss_intro_text);
+                mDismissOverlayView.showIntroIfNecessary();
+
+                mDetector = new GestureDetector(ContentActivity.this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public void onLongPress(MotionEvent e) {
+                        mDismissOverlayView.show();
+                    }
+                });
             }
         });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mDetector.onTouchEvent(event) || super.onTouchEvent(event);
     }
 }
